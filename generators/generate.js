bd9fcb7b40568daa1aff9951af585119d2298898
Sherard Bailey <sbailey@xogrp.com>
2015-10-06 12:01:47 -0400

    More generator play

--- generators/generate.js
+++ generators/generate.js
@@ -7,6 +7,15 @@ var generateSolarSystem = function* () {
   }
 }
 
-for (var planet of generateSolarSystem()) {
-  console.log(planet);
-}
+// for (var planet of generateSolarSystem()) {
+//   console.log(planet);
+// }
+
+var iterator = generateSolarSystem()
+console.log("iterator", iterator);
+
+var val = iterator.next();
+// this should be the value out of the iterator,
+// essentially and object with two keys: value and done
+console.log("iterator return value", val);
+
