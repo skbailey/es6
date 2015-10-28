461fa5f31d4b87e6a03a7d2c39ddf15f605bffe4
Sherard Bailey <sbailey@xogrp.com>
2015-10-27 20:55:46 -0400

    Add modules to es6 features
    I couldn't get modules to work in Node 4.0 but I'd like to keep
    a copy of this

---
+++ modules/destination.js
@@ -0,0 +1,11 @@
+"use strict";
+
+export class Destination {
+  constructor() {
+
+  }
+
+  getVisitors() {
+    console.log("Here is a list of all visitors");
+  }
+}
---
+++ modules/travel_agent.js
@@ -0,0 +1,6 @@
+"use strict";
+
+import { Destination } from "destination.js"
+
+var destination = new Destination();
+destination.getVisitors();
